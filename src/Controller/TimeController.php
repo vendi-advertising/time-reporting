0627e2789a38fdf70a7637cc0d0053e4f2aae2d2
Chris Haas <chris@vendiadvertising.com>
2022-10-18 18:28:29 -0500

    Not sure. Sorry

--- src/Controller/TimeController.php
+++ src/Controller/TimeController.php
@@ -6,6 +6,7 @@ use App\DTO\DayOfWeek;
 use App\Entity\User;
 use App\Repository\ClientRepository;
 use App\Repository\UserTimeEntryRepository;
+use DateTimeImmutable;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,12 +14,19 @@ use Symfony\Component\Security\Core\Security;
 
 class TimeController extends AbstractController
 {
-    #[Route('/app/time', name: 'time')]
-    public function index(ClientRepository $clientRepository, UserTimeEntryRepository $userTimeEntryRepository, Security $security): Response
+    #[Route('/app/time/{dateStart}', name: 'time')]
+    public function index(ClientRepository $clientRepository, UserTimeEntryRepository $userTimeEntryRepository, Security $security, ?int $dateStart = null): Response
     {
-        $dateTime = new \DateTimeImmutable();
-        $monday = $dateTime->modify(('Sunday' === $dateTime->format('l')) ? 'Monday last week' : 'Monday this week');
+        if (!$dateStart) {
+            $dateStartObj = new DateTimeImmutable;
+        } else {
+            $dateStartObj = DateTimeImmutable::createFromFormat('Ymd', (string)$dateStart);
+        }
+
+        $monday = $dateStartObj->modify('Monday this week');
         $friday = $monday->modify('+4 day');
+        $previousWeek = $monday->modify('-7 days');
+        $nextWeek = $monday->modify('+7 days');
 
         /** @var User $user */
         $user = $security->getUser();
@@ -44,6 +52,8 @@ class TimeController extends AbstractController
                     new DayOfWeek('Friday', $monday->modify('+4 day')->format('m/d'), $monday->modify('+4 day')->format('Y-m-d')),
                 ],
                 'userTimeEntries' => $userTimeEntriesArray,
+                'previousWeek' => $previousWeek->format('Ymd'),
+                'nextWeek' => $nextWeek->format('Ymd'),
             ]
         );
     }
